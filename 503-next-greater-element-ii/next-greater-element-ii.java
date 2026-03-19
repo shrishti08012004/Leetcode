6a93e68218b49c6bee51f56a6a706e2b5733debd
Shrishti Mishra <mishrashrishti0801@gmail.com>
2026-03-19 21:59:51 +0530

    Time: 15 ms (53.41%) | Memory: 48.4 MB (71.81%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,27 +1,63 @@
 import java.util.*;
 
-class Solution {
-    public int[] nextGreaterElements(int[] nums) {
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
 
-        int n = nums.length;
+//         int n = nums.length;
+//         int[] nge = new int[n];
+
+//         Stack<Integer> st = new Stack<>();
+
+//         for(int i = 2*n - 1; i >= 0; i--){
+
+//             int idx = i % n;
+
+//             while(!st.isEmpty() && st.peek() <= nums[idx]){
+//                 st.pop();
+//             }
+
+//             if(i < n){
+//                 if(st.isEmpty()) nge[idx] = -1;
+//                 else nge[idx] = st.peek();
+//             }
+
+//             st.push(nums[idx]);
+//         }
+
+//         return nge;
+//     }
+//  }
+
+class Solution {
+
+    public int[] nextGreaterElements(int[] arr) {
+
+        int n = arr.length;
         int[] nge = new int[n];
 
         Stack<Integer> st = new Stack<>();
 
-        for(int i = 2*n - 1; i >= 0; i--){
+        // Step 1: Fill stack (simulate circular)
+        for(int i = n - 1; i >= 0; i--){
+            st.push(arr[i]);
+        }
 
-            int idx = i % n;
+        // Step 2: Find next greater elements
+        for(int i = n - 1; i >= 0; i--){
 
-            while(!st.isEmpty() && st.peek() <= nums[idx]){
+            // remove smaller elements
+            while(!st.isEmpty() && arr[i] >= st.peek()){
                 st.pop();
             }
 
-            if(i < n){
-                if(st.isEmpty()) nge[idx] = -1;
-                else nge[idx] = st.peek();
-            }
+            // assign answer
+            if(st.isEmpty())
+                nge[i] = -1;
+            else
+                nge[i] = st.peek();
 
-            st.push(nums[idx]);
+            // push current element
+            st.push(arr[i]);
         }
 
         return nge;
