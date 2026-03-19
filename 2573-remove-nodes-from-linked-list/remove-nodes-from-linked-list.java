07e099f3343f2ee01bf78798499c2002d61b6b34
Shrishti Mishra <mishrashrishti0801@gmail.com>
2026-03-19 17:24:23 +0530

    Time: 7 ms (72.58%) | Memory: 130.5 MB (69.99%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -11,30 +11,72 @@
 class Solution {
     public ListNode removeNodes(ListNode head) {
 
-        Stack<ListNode> st = new Stack<>();
+        // Step 1: reverse the list
+        head = reverse(head);
 
-        ListNode temp = head;
+        // Step 2: keep max and remove smaller nodes
+        int max = head.val;
+        ListNode curr = head;
 
-        // Step 1: Process nodes using stack
-        while(temp != null){
+        while(curr != null && curr.next != null){
 
-            while(!st.isEmpty() && st.peek().val < temp.val){
-                st.pop();
+            if(curr.next.val < max){
+                curr.next = curr.next.next; // remove node
             }
-
-            st.push(temp);
-            temp = temp.next;
+            else{
+                curr = curr.next;
+                max = curr.val;
+            }
         }
 
-        // Step 2: Rebuild linked list
-        ListNode newHead = null;
+        // Step 3: reverse again
+        return reverse(head);
+    }
 
-        while(!st.isEmpty()){
-            ListNode node = st.pop();
-            node.next = newHead;
-            newHead = node;
+    // reverse function
+    private ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        return newHead;
+        return prev;
     }
 }
+
+
+// class Solution {
+//     public ListNode removeNodes(ListNode head) {
+
+//         Stack<ListNode> st = new Stack<>();
+
+//         ListNode temp = head;
+
+//         // Step 1: Process nodes using stack
+//         while(temp != null){
+
+//             while(!st.isEmpty() && st.peek().val < temp.val){
+//                 st.pop();
+//             }
+
+//             st.push(temp);
+//             temp = temp.next;
+//         }
+
+//         // Step 2: Rebuild linked list
+//         ListNode newHead = null;
+
+//         while(!st.isEmpty()){
+//             ListNode node = st.pop();
+//             node.next = newHead;
+//             newHead = node;
+//         }
+
+//         return newHead;
+//     }
+// }
