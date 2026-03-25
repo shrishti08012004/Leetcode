e46ae01994d1e715178f1b45754ca0424b223b18
Shrishti Mishra <mishrashrishti0801@gmail.com>
2026-03-26 05:10:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (94.04%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int writer= 0;
+            for(int reader=0; reader<nums.length; reader++) {
+                if(nums[reader]!= val){
+
+                nums[writer]=nums[reader];
+                writer++;
+            }
+        
+    }
+    return writer;
+}
+}
