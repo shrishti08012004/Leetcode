dd0f34837c055bce3e3d7ff8a40cecee3e03baf3
Shrishti Mishra <mishrashrishti0801@gmail.com>
2026-04-09 18:37:08 +0530

    Time: 0 ms (100.00%) | Memory: 80.4 MB (28.46%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,17 +1,46 @@
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+//         int s=0;
+//         int e=arr.length-1;
+
+//         while(s<e){
+//             int m= s+(e-s)/2;
+//             if(arr[m]>arr[m+1]){
+//                 e=m;
+//                 }
+//                 else{
+//                     s=m+1;
+//                 }
+//             }
+//             return s;
+//         }
+//     }
+
+
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int s=0;
-        int e=arr.length-1;
+        
+        int s = 1;                  // start from 1 (safe)
+        int e = arr.length - 2;     // end at n-2 (safe)
 
-        while(s<e){
-            int m= s+(e-s)/2;
-            if(arr[m]>arr[m+1]){
-                e=m;
-                }
-                else{
-                    s=m+1;
-                }
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            // ✅ Check if peak
+            if (arr[m] > arr[m - 1] && arr[m] > arr[m + 1]) {
+                return m;
             }
-            return s;
+            // 🔸 Increasing side → go right
+            else if (arr[m] > arr[m - 1]) {
+                s = m + 1;
+            }
+            // 🔸 Decreasing side → go left
+            else {
+                e = m - 1;
+            }
         }
+
+        return -1; // should not happen
     }
+}
+
