fc79d78115c109e72317542426d5d12cbb5fc9d1
Shrishti Mishra <mishrashrishti0801@gmail.com>
2026-04-10 22:09:52 +0530

    Time: 7 ms (45.70%) | Memory: 47.6 MB (5.87%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,16 +1,58 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap <Integer , Integer> map=new HashMap<>();
+//         int result[] = new int[2];
+//         for(int i=0;i<nums.length;i++){
+//             if(map.containsKey(target- nums[i])){
+//                 result[0]= map.get(target-nums[i]);
+//                 result[1]=i;
+//                 break;
+//             }
+//             map.put(nums[i],i);
+//         }
+//         return result;    
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer , Integer> map=new HashMap<>();
-        int result[] = new int[2];
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(target- nums[i])){
-                result[0]= map.get(target-nums[i]);
-                result[1]=i;
-                break;
+
+        int[] temp = nums.clone();
+        Arrays.sort(temp);
+
+        int i = 0, j = temp.length - 1;
+
+        while (i < j) {
+            int sum = temp[i] + temp[j];
+
+            if (sum == target) {
+                int first = temp[i];
+                int second = temp[j];
+
+                int idx1 = -1, idx2 = -1;
+
+                // find indices in original array
+                for (int k = 0; k < nums.length; k++) {
+                    if (nums[k] == first && idx1 == -1) {
+                        idx1 = k;
+                    } 
+                    else if (nums[k] == second && idx2 == -1) {
+                        idx2 = k;
+                    }
+                }
+
+                return new int[]{idx1, idx2};
+            } 
+            else if (sum < target) {
+                i++;
+            } 
+            else {
+                j--;
             }
-            map.put(nums[i],i);
         }
-        return result;
-        
+
+        return new int[]{-1, -1};
     }
 }
